d99445bf360dc1f78d6a0c40e64a3c243f57ae78
madhu2000u <49570614+madhu2000u@users.noreply.github.com>
2020-06-02 14:56:27 +0530

    Add files via upload

---
+++ piano tiles bot.py
@@ -0,0 +1,36 @@
+﻿from ppadb.client import Client
+import numpy
+from PIL import Image, ImageGrab
+
+
+adb=Client(host="127.0.0.1", port=5037)
+devices=adb.devices()
+
+device=devices[0]
+
+#device.shell("input tap 500 500")
+while(True):
+    image=ImageGrab.grab(bbox=None)
+
+    #im=numpy.array(image, dtype=numpy.uint8)
+    #for i in im:
+     #   print (i)
+
+
+   # with open('image.png', 'wb') as file:
+    #   file.write(image)
+
+    #image=Image.open('image.png')
+    image=numpy.array(image, dtype=numpy.uint8)
+    #print (image)
+    
+
+    pixels=[list(i[:3]) for i in image[800]]
+
+    for i, pixel in enumerate(pixels):
+        r, b,g=[int(i) for i in pixel]
+        #print (r, g, b)
+        if(r+g+b==0):
+            print("In if")
+            device.shell("input tap "+str(i)+" 800")
+            break
